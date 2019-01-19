df9e707bb3c0b32ca01a9164ccd40486f9e4051a
YourGitHubName <alan@wecancodeit.org>
2019-01-19 14:57:50 -0500

    refactor code to include Big Decimal

--- src/main/java/atm/flex/Atm.java
+++ src/main/java/atm/flex/Atm.java
@@ -1,37 +1,39 @@
 package atm.flex;
 
+import java.math.BigDecimal;
+
 public class Atm {
 
-	private int balance;
+	private BigDecimal balance;
 	private String pin;
 
-	public int getBalance() {
+	public BigDecimal getBalance() {
 		return balance;
 	}
 
-	public Atm(int balance, String pin) {
+	public Atm(BigDecimal balance, String pin) {
 		this.balance = balance;
 		this.pin = pin;
 	}
 
 	// method overloading: methods of same name, doing different things
 	public void deposit() {
-		balance += 10;
+		balance = balance.add(new BigDecimal("10.00"));
 	}
 
-	public void deposit(int depositAmt) {
-		balance += depositAmt;
+	public void deposit(BigDecimal depositAmt) {
+		balance = balance.add(depositAmt);
 	}
 
-	public void withdraw(int withdrawAmount) {
-		if (withdrawAmount > balance) {
-			balance -= 0;
+	public void withdraw(BigDecimal withdrawAmount) {
+		if (withdrawAmount.compareTo(balance) > 0) {
+			// balance -= 0;
 			System.out.println("Insufficient funds!");
-		} else if (withdrawAmount % 10 != 0) {
-			balance -= 0;
+		} else if (withdrawAmount.remainder(new BigDecimal("10.00")).compareTo(BigDecimal.ZERO) > 0) {
+			// balance -= 0;
 			System.out.println("Please enter multiples of 10");
 		} else {
-			balance -= withdrawAmount;
+			balance = balance.subtract(withdrawAmount);
 		}
 
 	}
@@ -43,7 +45,5 @@ public class Atm {
 			return false;
 		}
 	}
-
-	
 
 }
--- src/test/java/atm/flex/AtmTest.java
+++ src/test/java/atm/flex/AtmTest.java
@@ -3,71 +3,72 @@ package atm.flex;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.math.BigDecimal;
+
 import org.junit.Test;
 
 public class AtmTest {
-	
-	Atm underTest = new Atm(50, "1111");
-	
+
+	Atm underTest = new Atm(new BigDecimal("50.00"), "1111");
+
 	@Test
 	public void shouldHaveABalanceOf50() {
-		int expected = underTest.getBalance();
-		assertThat(expected, is(50));
+		BigDecimal expected = underTest.getBalance();
+		assertThat(expected, is(new BigDecimal("50.00")));
 	}
-	
+
 	@Test
 	public void shouldHaveABalanceOf100() {
-		Atm underTest = new Atm(100, "1111");
-		int expected = underTest.getBalance();
-		assertThat(expected, is(100));
+		Atm underTest = new Atm(new BigDecimal("100.00"), "1111");
+		BigDecimal expected = underTest.getBalance();
+		assertThat(expected, is(new BigDecimal("100.00")));
 	}
-	
+
 	@Test
 	public void defaultDepositShouldIncreaseBalanceFrom50To60() {
 		underTest.deposit();
-		int expected = underTest.getBalance();
-		assertThat(expected, is(60));
+		BigDecimal expected = underTest.getBalance();
+		assertThat(expected, is(new BigDecimal("60.00")));
 	}
-	
+
 	@Test
 	public void shouldIncreaseBalanceByUserRequestedAmount() {
-		underTest.deposit(25);
-		int expected = underTest.getBalance();
-		assertThat(expected, is(75));
+		underTest.deposit(new BigDecimal("25.00"));
+		BigDecimal expected = underTest.getBalance();
+		assertThat(expected, is(new BigDecimal("75.00")));
 	}
-	
+
 	@Test
 	public void shouldDecreaseBalanceByUserRequestedAmount() {
-		underTest.withdraw(10);
-		int expected = underTest.getBalance();
-		assertThat(expected, is(40));
+		underTest.withdraw(new BigDecimal("10.00"));
+		BigDecimal expected = underTest.getBalance();
+		assertThat(expected, is(new BigDecimal("40.00")));
 	}
-	
+
 	@Test
 	public void shouldDecreaseBalanceBy0WhenUserRequestIsOver() {
-		underTest.withdraw(100);
-		int expected = underTest.getBalance();
-		assertThat(expected, is(50));
+		underTest.withdraw(new BigDecimal("100.00"));
+		BigDecimal expected = underTest.getBalance();
+		assertThat(expected, is(new BigDecimal("50.00")));
 	}
-	
+
 	@Test
 	public void shouldDecreaseBalanceBy0WhenUserRequestIsNotMultipleOf10() {
-		underTest.withdraw(1);
-		int expected = underTest.getBalance();
-		assertThat(expected, is(50));
+		underTest.withdraw(new BigDecimal("1.00"));
+		BigDecimal expected = underTest.getBalance();
+		assertThat(expected, is(new BigDecimal("50.00")));
 	}
-	
+
 	@Test
 	public void shouldAllowUserAccessWithCorrectPin() {
 		boolean expected = underTest.accessAccount("1111");
 		assertThat(expected, is(true));
 	}
-	
+
 	@Test
 	public void shouldDenyUserAccessWithIncorrectPin() {
 		boolean expected = underTest.accessAccount("2222");
 		assertThat(expected, is(false));
 	}
-	
 
 }
